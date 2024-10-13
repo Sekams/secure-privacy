e5f25115f2b10593a1bd8830ed6368e11b8727c2
sekams <spssekamatte@gmail.com>
2024-10-13 13:57:53 -0400

    Add Product Catalogue Web App main project files

---
+++ ProductCatalogue/ProductCatalogue.App/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  API_KEY: 'my-api-key',
+  API_KEY_HEADER: 'X-Api-Key',
+  API_URL: 'http://localhost:5231/api',
+  DATA_OFFICER_EMAIL: 'spssekamate@gmail.com',
+  COUNTRY: 'United States',
+};
