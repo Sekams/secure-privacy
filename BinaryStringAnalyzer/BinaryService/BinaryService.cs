2239104c539849006a1eb53e58345a2cd2926a2c
sekams <spssekamatte@gmail.com>
2024-10-13 13:43:01 -0400

    Add Binary String Analyzer service files

---
+++ BinaryStringAnalyzer/BinaryService/BinaryService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BinaryStringAnalyzer.Services;
+
+public class BinaryService
+{
+    public bool IsGoodBinaryString(string binaryString)
+    {
+        if (string.IsNullOrWhiteSpace(binaryString)) {
+            return false;
+        }
+
+        int zeros = 0;
+        int ones = 0;
+
+        foreach (char c in binaryString)
+        {
+            if (c == '0')
+            {
+                zeros++;
+            }
+            else if (c == '1')
+            {
+                ones++;
+            }
+            else
+            {
+                return false;
+            }
+            if (ones < zeros){
+                return false;
+            }
+        }
+
+        return zeros == ones;
+    }
+}
